a573bcd93d24ef9fa1f7b536b226be325a07b8aa
SCHENLIU <cochen55@yeah.net>
2019-11-18 13:13:25 +0800

    Delete article.py

--- article.py
+++
@@ -1,7 +0,0 @@
-class article(object):
-    def __init__(self):
-        self.title = None
-        self.content = None
-        self.label = None
-        self.title_matched = None
-        self.content_matched = None
