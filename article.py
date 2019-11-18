0b77b85fc04636f440796db4a98d9fba9d50b3a0
SCHENLIU <cochen55@yeah.net>
2019-11-18 13:02:20 +0800

    Add files via upload

---
+++ article.py
@@ -0,0 +1,7 @@
+class article(object):
+    def __init__(self):
+        self.title = None
+        self.content = None
+        self.label = None
+        self.title_matched = None
+        self.content_matched = None
